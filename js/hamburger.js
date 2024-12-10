8149af8ff3588e92b36e51550f0da1b201337b64
vmdplacido <nina9avalon@gmail.com>
2024-12-10 14:45:44 -0600

    add

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,1 +1,29 @@
-console.log(hello);
+console.log("hello");
+
+const ul = document.querySelector("nav ul");
+
+console.log(ul);
+
+const burgerButton = document.getElementById("burger-button");
+
+console.log(burgerButton);
+
+console.log(burgerButton);
+
+const menuCloseIcon = document.getElementById("menu-close");
+
+const menuOpenIcon = document.getElementById("menu-open");
+console.log(menuOpenIcon);
+
+console.log(menuOpenIcon);
+
+function handleHamburgerButtonClick() {
+  console.log("button clicked");
+  console.log("button clicked");
+  ul.classList.toggle("visible");
+
+  menuCloseIcon.classList.toggle(visible);
+
+  menuOpenIcon.classList.toggle(visible);
+}
+burgerButton.addEventListener("click", handleHamburgerButtonClick);
